62d28bdfdfde2b27010d5c33783f749bed48d54d
John Edward Gonzalez <edscodes@gmail.com>
2023-07-27 22:37:04 -0500

    Add files via upload

--- js/kopenom.js
+++ js/kopenom.js
@@ -1,6 +1,18 @@
-function seleccionarMascotaJugador(){
-    alert("Seleccionaste tu mascota")
+function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+function seleccionarMascotaJugador(){
+    if (document.getElementById('Firekick').checked){
+        alert("Seleccionaste a firekick")}
+    else if (document.getElementById('Waterhit').checked)
+        alert("Seleccionaste a Waterhit")
+    else if (document.getElementById('Earthpunch').checked)
+        alert("Seleccionaste a Earthpunch")
+    else {
+        alert("por favor selecciona tu mascota para el combate")
+    }
+}
+
+window.addEventListener('load', iniciarJuego)
