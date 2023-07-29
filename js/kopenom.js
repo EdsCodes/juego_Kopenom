55cc6fee6248c824cea04028e822b27e26285d24
John Edward Gonzalez <edscodes@gmail.com>
2023-07-28 23:50:00 -0500

    Add files via upload

--- js/kopenom.js
+++ js/kopenom.js
@@ -4,15 +4,44 @@ function iniciarJuego(){
 }
 
 function seleccionarMascotaJugador(){
-    if (document.getElementById('Firekick').checked){
-        alert("Seleccionaste a firekick")}
-    else if (document.getElementById('Waterhit').checked)
-        alert("Seleccionaste a Waterhit")
-    else if (document.getElementById('Earthpunch').checked)
-        alert("Seleccionaste a Earthpunch")
+    let inputFirekick = document.getElementById('Firekick')
+    let inputWaterhit = document.getElementById('Waterhit')
+    let inputEarthpunch = document.getElementById('Earthpunch')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
+    
+
+    if (inputFirekick.checked){
+        spanMascotaJugador.innerHTML = 'Firekick'
+        alert("Seleccionaste a Firekick")}
+    else if (inputWaterhit.checked){
+        spanMascotaJugador.innerHTML = 'Waterhit'
+        alert("Seleccionaste a Waterhit")}
+    else if (inputEarthpunch.checked){
+        spanMascotaJugador.innerHTML = 'Earthpunch'
+        alert("Seleccionaste a Earthpunch")}
     else {
         alert("por favor selecciona tu mascota para el combate")
     }
 }
 
+function seleccionarMascotaEnemigo(){
+    
+    let MascotaEnemigo = ''
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+    function aleatorio(min, max){
+        return Math.floor(Math.random() * (max-min+1) + min)
+    }
+
+    function eleccionMascotaEnemigo(jugadaPc) {
+        if(aleatorio == 1) {
+            jugadaPc = "Firekick"
+        } else if (aleatorio == 2) {
+            jugadaPc = "Waterhit"
+        } else if (aleatorio == 3) {
+            jugadaPc = "Earthpunch"
+        } 
+        return MascotaEnemigo
+    }
+}
 window.addEventListener('load', iniciarJuego)
