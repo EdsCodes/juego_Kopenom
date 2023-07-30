f3f5d8f07f93ee45c9b93d0715eac05005a2ba91
John Edward Gonzalez <edscodes@gmail.com>
2023-07-30 00:58:03 -0500

    Add files via upload

--- js/kopenom.js
+++ js/kopenom.js
@@ -3,71 +3,44 @@ function iniciarJuego(){
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
-function seleccionarMascotaJugador(){
-    let inputFirekick = document.getElementById('Firekick')
-    let inputWaterhit = document.getElementById('Waterhit')
-    let inputEarthpunch = document.getElementById('Earthpunch')
-    let spanMascotaJugador = document.getElementById('mascota-jugador')
+    function seleccionarMascotaJugador(){
+        let inputFirekick = document.getElementById('Firekick')
+        let inputWaterhit = document.getElementById('Waterhit')
+        let inputEarthpunch = document.getElementById('Earthpunch')
+        let spanMascotaJugador = document.getElementById('mascota-jugador')
     
 
-    if (inputFirekick.checked){
-        spanMascotaJugador.innerHTML = 'Firekick'
-        alert("Seleccionaste a Firekick")}
-    else if (inputWaterhit.checked){
-        spanMascotaJugador.innerHTML = 'Waterhit'
-        alert("Seleccionaste a Waterhit")}
-    else if (inputEarthpunch.checked){
-        spanMascotaJugador.innerHTML = 'Earthpunch'
-        alert("Seleccionaste a Earthpunch")}
-    else {
-        alert("por favor selecciona tu mascota para el combate")
-    } return MascotaJugador
-}
+        if (inputFirekick.checked){
+                spanMascotaJugador.innerHTML = 'Firekick'
+                alert("Seleccionaste a Firekick")
+            }else if (inputWaterhit.checked){
+                spanMascotaJugador.innerHTML = 'Waterhit'
+                alert("Seleccionaste a Waterhit")
+            }else if (inputEarthpunch.checked){
+                spanMascotaJugador.innerHTML = 'Earthpunch'
+                alert("Seleccionaste a Earthpunch")
+            }else {
+                alert("por favor selecciona tu mascota para el combate")
+            } 
 
-function seleccionarMascotaEnemigo(){
-    
-    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
-
-    function aleatorio(min, max){
-        return Math.floor(Math.random() * (max-min+1) + min)
-    }
-    function eleccion(jugada) {
-        let MascotaEnemigo = ""
-        if(jugada == 1) {
-            MascotaEnemigo = "Firekick"
-        } else if (jugada == 2) {
-            MascotaEnemigo = "Waterhit"
-        } else if (jugador == 3) {
-            MascotaEnemigo = "Earthpunch"
+            seleccionarMascotaEnemigo()
         }
-        return MascotaEnemigo
+
+        function aleatorio(min, max){
+            return Math.floor(Math.random() * (max-min+1) + min)
+        }
+
+        function seleccionarMascotaEnemigo(){
+            let ataqueAleatorio = aleatorio(1,3)
+            let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+            if (ataqueAleatorio == 1){
+                   spanMascotaEnemigo.innerHTML = "Firekick"
+               } else if (ataqueAleatorio == 2){
+               spanMascotaEnemigo.innerHTML = "Waterhit" 
+                } else {
+                spanMascotaEnemigo.innerHTML = "Earthpunch"
+            }
     }
-
-    let pc = 0
-    let triunfos = 0
-    let perdidas = 0
-
-    while (triunfos < 3 && perdidas < 3) {
-    pc = aleatorio(1,3)
-    alert("Eligiste: " + seleccionarMascotaJugador(jugador))  
-    alert("PC eligio: " + eleccion(pc)) 
-
-    //combate
-    if(pc == jugador) {
-        alert("Empate")
-        } else if((jugador == 1 && pc == 3) || (jugador == 2 && pc == 1) || (jugador == 3 && pc == 2)){
-            alert("Ganaste")
-            triunfos = triunfos + 1
-        } else {
-            alert("Perdiste")
-            perdidas = perdidas + 1
-    }
-}
-
-if(triunfos == 1) {
-alert("Ganaste " + triunfos + " vez y perdiste " + perdidas + " veces.")
-} else {
-alert("Ganaste " + triunfos + " veces y perdiste " + perdidas + " veces.")
-}
-}
+       
 window.addEventListener('load', iniciarJuego)
