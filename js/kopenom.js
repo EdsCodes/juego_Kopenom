f50286afe9e2aa56b1178164d570f2c6fd5f19e4
John Edward Gonzalez <edscodes@gmail.com>
2023-07-30 00:24:20 -0500

    Add files via upload

--- js/kopenom.js
+++ js/kopenom.js
@@ -21,27 +21,53 @@ function seleccionarMascotaJugador(){
         alert("Seleccionaste a Earthpunch")}
     else {
         alert("por favor selecciona tu mascota para el combate")
-    }
+    } return MascotaJugador
 }
 
 function seleccionarMascotaEnemigo(){
     
-    let MascotaEnemigo = ''
     let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
 
     function aleatorio(min, max){
         return Math.floor(Math.random() * (max-min+1) + min)
     }
-
-    function eleccionMascotaEnemigo(jugadaPc) {
-        if(aleatorio == 1) {
-            jugadaPc = "Firekick"
-        } else if (aleatorio == 2) {
-            jugadaPc = "Waterhit"
-        } else if (aleatorio == 3) {
-            jugadaPc = "Earthpunch"
-        } 
+    function eleccion(jugada) {
+        let MascotaEnemigo = ""
+        if(jugada == 1) {
+            MascotaEnemigo = "Firekick"
+        } else if (jugada == 2) {
+            MascotaEnemigo = "Waterhit"
+        } else if (jugador == 3) {
+            MascotaEnemigo = "Earthpunch"
+        }
         return MascotaEnemigo
     }
+
+    let pc = 0
+    let triunfos = 0
+    let perdidas = 0
+
+    while (triunfos < 3 && perdidas < 3) {
+    pc = aleatorio(1,3)
+    alert("Eligiste: " + seleccionarMascotaJugador(jugador))  
+    alert("PC eligio: " + eleccion(pc)) 
+
+    //combate
+    if(pc == jugador) {
+        alert("Empate")
+        } else if((jugador == 1 && pc == 3) || (jugador == 2 && pc == 1) || (jugador == 3 && pc == 2)){
+            alert("Ganaste")
+            triunfos = triunfos + 1
+        } else {
+            alert("Perdiste")
+            perdidas = perdidas + 1
+    }
+}
+
+if(triunfos == 1) {
+alert("Ganaste " + triunfos + " vez y perdiste " + perdidas + " veces.")
+} else {
+alert("Ganaste " + triunfos + " veces y perdiste " + perdidas + " veces.")
+}
 }
 window.addEventListener('load', iniciarJuego)
