56dd7e38e1280c5391839f5612ee1dddcbd8e675
John Edward Gonzalez <edscodes@gmail.com>
2023-07-30 22:53:46 -0500

    Add files via upload
    Se cambian los nombres de kopenom.html por index_kopenom, se crea los archivos aparte y se modula el código.

---
+++ js/combate_kopenom.js
@@ -0,0 +1,14 @@
+function ataqueFuego(){
+    ataqueJugador = 'fuego'
+    alert(ataqueJugador)
+}
+function ataqueAgua(){
+    ataqueJugador = 'agua'
+    alert(ataqueJugador)
+}
+function ataqueTierra(){
+    ataqueJugador = 'tierra'
+    alert(ataqueJugador)
+}
+
+Let AtaqueAleatorio = ''
